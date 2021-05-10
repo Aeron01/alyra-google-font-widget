c5d9a868e805940a9d9e71477dd4875d439a13f4
Aeron01 <one.soulreaper@gmail.com>
2021-05-10 09:44:33 +0200

    done and working

--- src/components/TextSize.js
+++ src/components/TextSize.js
@@ -4,6 +4,10 @@ const TextSize = (props) => {
   return (
     <>
       <label htmlFor="customRange1" className="form-label fw-bold mb-3">La taille de police</label>
+      <span
+        className="btn btn-danger btn-circle btn-md p-0"
+        style={{ width: '30px', height: '30px', borderRadius: '15px', fontSize: '18px' }}>
+        {textSize}</span>
       <input onChange={handleOnChange} type="range" className="form-range" id="customRange1" min="8" max="48" step="1" value={textSize} />
     </>
   );
